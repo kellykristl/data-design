265d622b8be152cdf0ab181c3387366c1447ec13
kellykristl <goddesswebdesign@gmail.com>
2017-04-23 08:47:43 -0600

    pushing initial files

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -6,7 +6,7 @@ profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 profileActivationToken CHAR(32),
 profileAtHandle VARCHAR(32) NOT NULL,
 -- to make sure duplicate data cannot exist, create a unique index
-profileEmail VARCHAR(128) UNIQUE NOT NULL,
+profileEmail VARCHAR(128) NOT NULL,
 profileHash CHAR(128) NOT NULL,
 -- to make something optional, exclude the not null
 profilePhone VARCHAR(32),
@@ -17,33 +17,32 @@ UNIQUE(profileAtHandle),
 PRIMARY KEY(profileId)
 );
 
-CREATE TABLE tweet (
+CREATE TABLE product (
 -- this is for yet another primary key...
-tweetId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 -- this is for a foreign key; auto_incremented is omitted by design
-tweetProfileId INT UNSIGNED NOT NULL,
-tweetContent VARCHAR(140) NOT NULL,
--- notice dates don't need a size parameter
-tweetDate DATETIME NOT NULL,
+productProfileId INT UNSIGNED NOT NULL,
+productContent VARCHAR(140) NOT NULL,
+productDate DATETIME(6) NOT NULL,
 -- this creates an index before making a foreign key
-INDEX(tweetProfileId),
+INDEX(productProfileId),
 -- this creates the actual foreign key relation
-FOREIGN KEY(tweetProfileId) REFERENCES profile(profileId),
+FOREIGN KEY(productProfileId) REFERENCES profile(profileId),
 -- and finally create the primary key
-PRIMARY KEY(tweetId)
+PRIMARY KEY(productId)
 );
 
-CREATE TABLE 'like' (
+CREATE TABLE favorite (
 -- these are not auto_increment because they're still foreign keys
-likeProfileId INT UNSIGNED NOT NULL,
-likeTweetId INT UNSIGNED NOT NULL,
-likeDate DATETIME NOT NULL,
+favoriteProfileId INT UNSIGNED NOT NULL,
+favoriteProductId INT UNSIGNED NOT NULL,
+favoriteDate DATETIME(6) NOT NULL,
 -- index the foreign keys
-INDEX(likeProfiileId),
-INDEX(likeTweetId),
+INDEX(favoriteProfileId),
+INDEX(favoriteProductId),
 -- create the foreign key relations
-FOREIGN KEY(likeProfileId) REFERENCES profile(profileId),
-FOREIGN KEY(likeTweetId) REFERENCES tweet(tweetId),
+FOREIGN KEY(favoriteProfileId) REFERENCES profile(profileId),
+FOREIGN KEY(favoriteProductId) REFERENCES product(productId),
 -- finally, create a composite foreign key with the two foreign keys
-PRIMARY KEY(likeProfileId, likeTweetId)
+PRIMARY KEY(favoriteProfileId, favoriteProductId)
 );
